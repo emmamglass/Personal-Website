cae0adda15f35a0e730d0ca91bdef40782769319
Emma M Glass <tfz5vy@virginia.edu>
2022-08-09 14:54:01 -0400

    updated files

--- dropdowns/dropdown.tsx
+++ dropdowns/dropdown.tsx
@@ -13,8 +13,8 @@ export const papinLab = () => {
         pathogen relatedness. I utilized machine learning techniques such as dimensionality reduction
         via t-SNE and linear regression to analyze the GENREs.
       </p>
-      <a href='/files/Ecological-Niche-Constrains-Evolution-of-Metabolic-Function-in-Pathobionts.pdf' target='_blank'>Abstract</a>
-      <a href='https://github.com/emmamglass/PATHGENN' target='_blank'>View Source</a>
+      <a href='/files/Ecological-Niche-Constrains-Evolution-of-Metabolic-Function-in-Pathobionts.pdf' target='_blank' rel='noopener noreferrer'>Abstract</a>
+      <a href='https://github.com/emmamglass/PATHGENN' target='_blank' rel='noopener noreferrer'>View Source</a>
       <h4><i>Metabolic Analysis of the Gardnerella Pangenome in silico</i> <span>(Manuscript Under Review)</span></h4>
       <p>
         I worked with a senior graduate student to characterize the pangenome of Gardnerella, the
@@ -22,8 +22,8 @@ export const papinLab = () => {
         reconstructions of all sequenced Gardnerella strains. We then utilized machine learning
         methods such as dimensionality reduction via t-SNE, hierarchical clustering, and random forest.
       </p>
-      <a href='/files/Metabolic-Analysis-of-the-Gardnerella-Pangenome-in-silico.pdf' target='_blank'>Abstract</a>
-      <a href='https://github.com/emmamglass/Gardnerella_Pangenome' target='_blank'>View Source</a>
+      <a href='/files/Metabolic-Analysis-of-the-Gardnerella-Pangenome-in-silico.pdf' target='_blank' rel='noopener noreferrer'>Abstract</a>
+      <a href='https://github.com/emmamglass/Gardnerella_Pangenome' target='_blank' rel='noopener noreferrer'>View Source</a>
       <h4><i>Reconstructor: a COBRApy compatible tool for automated genome scale metabolic network reconstructions through parsimonious flux-based gap-filling</i> <span>(Manuscript in Preparation)</span></h4>
       <p>
         I worked with a Post Doc to develop and package a novel tool, Reconstructor, that utilizes a
@@ -31,7 +31,7 @@ export const papinLab = () => {
         network reconstructions. I packaged the tool and have made it available through PyPi and on
         Github.
       </p>
-      <a href='https://github.com/emmamglass/reconstructor' target='_blank'>View Source</a>
+      <a href='https://github.com/emmamglass/reconstructor' target='_blank' rel='noopener noreferrer'>View Source</a>
       <h4><i>Comparative Genomics and Phenomics Reveal Genetic and Functional Diversity within Pseudomonas aeruginosa Isolates</i> <span>(Manuscript in Preparation)</span></h4>
       <p>
         I am working on phenotypically profiling 25 clinical isolates of Pseudomonas aeruginosa using
@@ -39,7 +39,7 @@ export const papinLab = () => {
         used in conjunction with genome sequence data, rna sequence data, and metabolomics data to
         create a very well characterized set of clinical isolates.
       </p>
-      <a href='/files/Comparative-Genomics-and-Phenomics-Reveal-Genetic-and-Functional-Diversity-within-Pseudomonas-aeruginosa-Isolates.pdf' target='_blank'>Abstract</a>
+      <a href='/files/Comparative-Genomics-and-Phenomics-Reveal-Genetic-and-Functional-Diversity-within-Pseudomonas-aeruginosa-Isolates.pdf' target='_blank' rel='noopener noreferrer'>Abstract</a>
     </div>
   )
 }
@@ -57,8 +57,8 @@ export const raviLab = () => {
         with Dr. Radhakrishnan over the following two years, and ultimately publishing my second first
         author paper.
       </p>
-      <a href='/files/fmedt-04-934015.pdf' target='_blank'>Paper</a>
-      <a href='/files/BMES_Poster_2020-2.pdf' target='_blank'>Poster</a>
+      <a href='/files/fmedt-04-934015.pdf' target='_blank' rel='noopener noreferrer'>Paper</a>
+      <a href='/files/BMES_Poster_2020-2.pdf' target='_blank' rel='noopener noreferrer'>Poster</a>
     </div>
   )
 }
@@ -74,7 +74,7 @@ export const smithLab = () => {
         interaction energies between CaM-bound (i.e., primed) and phosphorylated subunits. We
         investigated these questions using graph theoretical and statistical mechanics approaches.
       </p>
-      <a href='/files/Research_Notes__CaMKII_Project.pdf' target='_blank'>Research Notes</a>
+      <a href='/files/Research_Notes__CaMKII_Project.pdf' target='_blank' rel='noopener noreferrer'>Research Notes</a>
     </div>
   )
 }
@@ -85,13 +85,13 @@ export const chitnisLab = () => {
       <h4><i>Visualizing Compound Action Potential Propagation With High Frequency Ultrasound</i></h4>
       <p>
         While working on branching into the biomedical sciences, I worked as a summer undergraduate
-        research intern in Dr. Parag Chitnis' Biomedical Imaging Lab at George Mason University during
+        research intern in Dr. Parag Chitnis&#39; Biomedical Imaging Lab at George Mason University during
         the summer of 2018 on a project that had the goal of using ultrasound imaging to visualize
         neural activity in lobster nerves. This project was the first step in working toward developing
         non-invasive electrophysiological recording technologies by utilizing high frequency ultrasound
         imaging techniques.
       </p>
-      <a href='/files/2018assipposterEmmaGlass.pdf' target='_blank'>Poster</a>
+      <a href='/files/2018assipposterEmmaGlass.pdf' target='_blank' rel='noopener noreferrer'>Poster</a>
     </div>
   )
 }
@@ -102,14 +102,14 @@ export const ferreriaLab = () => {
       <h4><i>Potential of marshes to attenuate storm surge water level in the Chesapeake Bay</i> <span>(Published)</span></h4>
       <p>
         I worked initially as a high school summer research intern during the summer of 2016 in Dr.
-        Celso Ferreria's Flood Hazards Research Lab as George Mason University, but our
+        Celso Ferreria&#39;s Flood Hazards Research Lab as George Mason University, but our
         collaboration continued for the following year an a half. I focused on analyzing and interpreting
         water level data to quantify the ability of the Chesapeake Bay wetlands to attenuate storm surge
         water levels. Using previous collected longitudinal data in excel files, I utilized MATLAB to
         quantify water-level attenuation over several years. Ultimately, this project became my first first
         author paper, and was pulished in the Journal of Limnology and Oceanography.
       </p>
-      <a href='/files/Liminology-Oceanography.pdf' target='_blank'>Paper</a>
+      <a href='/files/Liminology-Oceanography.pdf' target='_blank' rel='noopener noreferrer'>Paper</a>
     </div>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -78,13 +78,13 @@ const Home: NextPage = () => {
         </h2>
 
         <p>
-          I am a Biomedical Engineering PhD student at the University of Virginia working in the <a href='https://www.papinlab.org/' target='_blank'>Computational Systems Biology Lab</a> under <a href='https://engineering.virginia.edu/faculty/jason-papin' target='_blank'>Dr. Jason Papin</a>. Prior to graduate school, I completed my undergraduate degree in <a href='https://www.wm.edu/as/cams/index.php' target='_blank'>Computational and Applied Mathematics and Statistics (CAMS)</a> at William and Mary in December of 2020.
+          I am a Biomedical Engineering PhD student at the University of Virginia working in the <a href='https://www.papinlab.org/' target='_blank' rel='noopener noreferrer'>Computational Systems Biology Lab</a> under <a href='https://engineering.virginia.edu/faculty/jason-papin' target='_blank' rel='noopener noreferrer'>Dr. Jason Papin</a>. Prior to graduate school, I completed my undergraduate degree in <a href='https://www.wm.edu/as/cams/index.php' target='_blank' rel='noopener noreferrer'>Computational and Applied Mathematics and Statistics (CAMS)</a> at William and Mary in December of 2020.
         </p> 
         <p>
           I am interested in combining computational systems biology, biomedical data science, and mathematical modeling approaches to address biological questions implicated in human health. In my graduate studies, I am specifically interested in better understanding the mechanisms of pathogenic bacteria by exploring functional metabolism, long-term phenotypic evolution, and the impact of environment on pathogen adaptation.
         </p>
         <p>
-          My graduate research has been funded by the <a href='https://www.nsfgrfp.org/' target='_blank'>NSF Graduate Research Fellowship (GRFP)</a>, <a href='https://engineering.virginia.edu/news/2016/04/nih-t32-training-grant-biomedical-data-sciences' target='_blank'>NIH T32 Biomedical Data Sciences Training Grant</a>, and was awarded the UVA Distinguished Fellowship.
+          My graduate research has been funded by the <a href='https://www.nsfgrfp.org/' target='_blank' rel='noopener noreferrer'>NSF Graduate Research Fellowship (GRFP)</a>, <a href='https://engineering.virginia.edu/news/2016/04/nih-t32-training-grant-biomedical-data-sciences' target='_blank' rel='noopener noreferrer'>NIH T32 Biomedical Data Sciences Training Grant</a>, and was awarded the UVA Distinguished Fellowship.
         </p>
 
         <h2 style={{ marginBottom: '4px' }}>
@@ -93,31 +93,31 @@ const Home: NextPage = () => {
 
         <button onClick={handlePapin}>
           <img src='dropdown.png' className={` ${styles.arrow} ${papin ? styles.rotated : ''}` }/>
-          <p>Jason Papin's Computational Systems Biology Lab, UVA {'(2021-Present)'}</p>
+          <p>Jason Papin&#39;s Computational Systems Biology Lab, UVA {'(2021-Present)'}</p>
         </button>
         {papin ? papinLab() : ''}
         <br/>
         <button onClick={handleRadhakrishnan}>
           <img src='dropdown.png' className={` ${styles.arrow} ${rad ? styles.rotated : ''}` }/>
-          <p>Ravi Radhakrishnan's Biomolecular Systems Lab, UPenn {'(2019-2021)'}</p>
+          <p>Ravi Radhakrishnan&#39;s Biomolecular Systems Lab, UPenn {'(2019-2021)'}</p>
         </button>
         {rad ? raviLab() : ''}
         <br/>
         <button onClick={handleConradi}>
           <img src='dropdown.png' className={` ${styles.arrow} ${con ? styles.rotated : ''}` }/>
-          <p>Greg Conradi Smith's Computational Biology Lab, {'W&M (2018-2019)'}</p>
+          <p>Greg Conradi Smith&#39;s Computational Biology Lab, {'W&M (2018-2019)'}</p>
         </button>
         {con ? smithLab() : ''}
         <br/>
         <button onClick={handleChitnis}>
           <img src='dropdown.png' className={` ${styles.arrow} ${chit ? styles.rotated : ''}` }/>
-          <p>Parag Chitnis' Biomedical Imaging Lab, GMU {'(Summer 2018)'}</p>
+          <p>Parag Chitnis&#39; Biomedical Imaging Lab, GMU {'(Summer 2018)'}</p>
         </button>
         {chit ? chitnisLab() : ''}
         <br/>
         <button onClick={handleFerreria}>
           <img src='dropdown.png' className={` ${styles.arrow} ${fer ? styles.rotated : ''}` }/>
-          <p>Celso Ferreria's Flood Hazards Research Lab, GMU {'(2016-2017)'}</p>
+          <p>Celso Ferreria&#39;s Flood Hazards Research Lab, GMU {'(2016-2017)'}</p>
         </button>
         {fer ? ferreriaLab() : ''}
       </div>
